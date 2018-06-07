9eeb4392f778d1c9b04882387339c82aec3a475f
Shrimadhav U K <ovhvps@shrimadhavuk.me>
2018-06-07 18:17:32 +0530

    make reply_to reply to the original message instead of my message

--- tg_bot/modules/cust_filters.py
+++ tg_bot/modules/cust_filters.py
@@ -149,6 +149,11 @@ def reply_filter(bot: Bot, update: Update):
     if not to_match:
         return
 
+    # my custom thing
+    if message.reply_to_message:
+        message = message.reply_to_message
+    # my custom thing
+
     chat_filters = sql.get_chat_triggers(chat.id)
     for keyword in chat_filters:
         pattern = r"( |^|[^\w])" + re.escape(keyword) + r"( |$|[^\w])"
