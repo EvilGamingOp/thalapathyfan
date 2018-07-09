48b151e5a0ed2d559f1e942f4cf241478d1f9608
Shrimadhav U K <ovhvps@shrimadhavuk.me>
2018-07-09 19:41:42 +0530

    add condition check for should disable webpage preview

--- tg_bot/modules/cust_filters.py
+++ tg_bot/modules/cust_filters.py
@@ -176,9 +176,13 @@ def reply_filter(bot: Bot, update: Update):
                 keyb = build_keyboard(buttons)
                 keyboard = InlineKeyboardMarkup(keyb)
 
+                should_preview_disabled = True
+                if "telegra.ph" in filt.reply:
+                    should_preview_disabled = False
+
                 try:
                     message.reply_text(filt.reply, parse_mode=ParseMode.MARKDOWN,
-                                       disable_web_page_preview=True,
+                                       disable_web_page_preview=should_preview_disabled,
                                        reply_markup=keyboard)
                 except BadRequest as excp:
                     if excp.message == "Unsupported url protocol":
