8d8dee399ae28c8d1c6094136055fb227d362471
Shrimadhav U K <ovhvps@shrimadhavuk.me>
2018-07-26 22:57:27 +0530

    take @Shabier code

--- tg_bot/modules/msg_deleting.py
+++ tg_bot/modules/msg_deleting.py
@@ -47,7 +47,8 @@ def purge(bot: Bot, update: Update, args: List[str]) -> str:
                 elif err.message != "Message to delete not found":
                     LOGGER.exception("Error while purging chat messages.")
 
-            bot.send_message(chat.id, "Purge complete.")
+            # Inspired by #28 on upstream
+            # bot.send_message(chat.id, "Purge complete.")
             return "<b>{}:</b>" \
                    "\n#PURGE" \
                    "\n<b>Admin:</b> {}" \
